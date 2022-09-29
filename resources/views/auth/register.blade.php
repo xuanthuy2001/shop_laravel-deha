8dedfb3734f249718ea3e2cc618ce5fedf980d9a
thuy <nguyenxt01012001@gmail.com>
2022-09-29 23:48:36 +0700

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,40 @@
-@extends('client.layouts.app')
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <title>shop</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+    <meta name="csrf-token" content="hGMtTjtqWxpCexuYSYU56IeL0FZYjMHQ2kmkqDii">
+    <link href="http://127.0.0.1:8000/client/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="http://127.0.0.1:8000/client/css/style.css" rel="stylesheet">
+    <style>
+        .parent_eye{
+        position: relative;
+        display: inline-block;
+        }
+        .parent_eye input {
+            padding-right: 2.4em;
+            height: 2em;
+          }
 
-@section('content')
+          .parent_eye  input::-ms-reveal {
+            display: none;
+          }
+         .parent_eye span {
+            position: absolute;
+            right: 14px;
+            top: 45%;
+            transform: translateY(-50%);
+            z-index: 100;
+          }
+         .parent_eye span svg {
+            display: block;
+            padding: .2em;
+            width: 1.3em;
+            height: 1.3em;
+          }
+    </style>
+<body>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8" style="padding: 0">
@@ -74,9 +108,11 @@
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 parent_eye">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <span>
+                                    <svg id="togglePassword"   style="cursor: pointer"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.015 7c4.751 0 8.063 3.012 9.504 4.636-1.401 1.837-4.713 5.364-9.504 5.364-4.42 0-7.93-3.536-9.478-5.407 1.493-1.647 4.817-4.593 9.478-4.593zm0-2c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 3c-2.209 0-4 1.792-4 4 0 2.209 1.791 4 4 4s4-1.791 4-4c0-2.208-1.791-4-4-4z"/></svg>
+                                  </span>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -106,4 +142,20 @@
         </div>
     </div>
 </div>
-@endsection
+
+</body>
+<script>
+    const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#password");
+
+    togglePassword.addEventListener("click", function () {
+    
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    // toggle the eye icon
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+    });
+</script>
+</html>
--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -5,90 +5,9 @@
 
 @section('content')
 <div class="container-fluid mb-5">
-      <div class="row border-top px-xl-5">
+      <div class="row  px-xl-5">
             <div class="col-lg-3">
-                  <!-- Shop Sidebar Start -->
-                  <div class=" col-md-12">
-                      <!-- Price Start -->
-                      <div class="border-bottom mb-4 pb-4">
-                          <h5 class="font-weight-semi-bold mb-4">Filter by price</h5>
-                          <form class="options">
-                              <button>fillter</button>
-                              <div class="custom-control custom-radio d-flex align-items-center justify-content-between mb-3">
-                                  <input type="radio" name = "filter_price" class="custom-control-input" checked id="price-all">
-                                  <label class="custom-control-label" for="price-all">All Price</label>
-                                  <span class="badge border font-weight-normal">1000</span>
-                              </div>
-                              <div class="custom-control custom-radio d-flex align-items-center justify-content-between mb-3">
-                                  <input type="radio" name = "filter_price" class="custom-control-input" id="price-1" value = "[0,100]">
-                                  <label class="custom-control-label" for="price-1" value = "[0,100]">$0 - $100</label>
-                                  <span class="badge border font-weight-normal">150</span>
-                              </div>
-                              <div class="custom-control  custom-radio d-flex align-items-center justify-content-between mb-3">
-                                  <input type="radio" name="filter_price" class="custom-control-input" id="price-2">
-                                  <label class="custom-control-label" for="price-2">$100 - $200</label>
-                                  <span class="badge border font-weight-normal">295</span>
-                              </div>
-                              <div class="custom-control  custom-radio d-flex align-items-center justify-content-between mb-3">
-                                  <input type="radio" name="filter_price" class="custom-control-input" id="price-3">
-                                  <label class="custom-control-label" for="price-3">$200 - $300</label>
-                                  <span class="badge border font-weight-normal">246</span>
-                              </div>
-                              <div class="custom-control  custom-radio d-flex align-items-center justify-content-between mb-3">
-                                  <input type="radio" name="filter_price" class="custom-control-input" id="price-4">
-                                  <label class="custom-control-label" for="price-4">$300 - $400</label>
-                                  <span class="badge border font-weight-normal">145</span>
-                              </div>
-                              <div class="custom-control  custom-radio d-flex align-items-center justify-content-between">
-                                  <input type="radio" name="filter_price" class="custom-control-input" id="price-5">
-                                  <label class="custom-control-label" for="price-5">$400 - $500</label>
-                                  <span class="badge border font-weight-normal">168</span>
-                              </div>
-                          </form>
-                      </div>
-                      <!-- Price End -->
-      
-                     
-      
-                      <!-- Size Start -->
-                      <div class="mb-5">
-                          <h5 class="font-weight-semi-bold mb-4">Filter by size</h5>
-                          <form>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                                  <input type="checkbox" class="custom-control-input" checked id="size-all">
-                                  <label class="custom-control-label" for="size-all">All Size</label>
-                                  <span class="badge border font-weight-normal">1000</span>
-                              </div>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                                  <input type="checkbox" class="custom-control-input" id="size-1">
-                                  <label class="custom-control-label" for="size-1">XS</label>
-                                  <span class="badge border font-weight-normal">150</span>
-                              </div>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                                  <input type="checkbox" class="custom-control-input" id="size-2">
-                                  <label class="custom-control-label" for="size-2">S</label>
-                                  <span class="badge border font-weight-normal">295</span>
-                              </div>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                                  <input type="checkbox" class="custom-control-input" id="size-3">
-                                  <label class="custom-control-label" for="size-3">M</label>
-                                  <span class="badge border font-weight-normal">246</span>
-                              </div>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                                  <input type="checkbox" class="custom-control-input" id="size-4">
-                                  <label class="custom-control-label" for="size-4">L</label>
-                                  <span class="badge border font-weight-normal">145</span>
-                              </div>
-                              <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between">
-                                  <input type="checkbox" class="custom-control-input" id="size-5">
-                                  <label class="custom-control-label" for="size-5">XL</label>
-                                  <span class="badge border font-weight-normal">168</span>
-                              </div>
-                          </form>
-                      </div>
-                      <!-- Size End -->
-                  </div>
-                  <!-- Shop Sidebar End -->
+                
             </div>
 <div class="col-lg-9">
       <!-- Featured Start -->
